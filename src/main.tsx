984ea59b8566fca6f818af44e50be01a39e1a506
David-Itzhak-Kohen <114672677+David-Itzhak-Kohen@users.noreply.github.com>
2023-10-04 20:15:49 +0300

    Basic Bootstrap

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+
+const container = document.getElementById("root") as HTMLElement;
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => {
+  return {
+    define: {
+      "process.env": {},
+    },
+    server: {
+      open: true,
+      port: 3000,
+    },
+    build: {
+      outDir: "build",
+    },
+    plugins: [
+      react({
+        jsxImportSource: "@emotion/react",
+        babel: {
+          plugins: ["@emotion/babel-plugin"],
+        },
+      }),
+    ],
+  };
+});
