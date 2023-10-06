ff172edffcb9b098dbe377289dbcf0cb50a40938
David-Itzhak-Kohen <114672677+David-Itzhak-Kohen@users.noreply.github.com>
2023-10-06 16:54:30 +0300

    dded device context

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
+    base: "./",
     define: {
       "process.env": {},
     },
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, "./src"),
+      },
+    },
     server: {
-      open: true,
+      host: "localhost",
       port: 3000,
+      open: true,
     },
     build: {
+      sourcemap: true,
       outDir: "build",
     },
     plugins: [
