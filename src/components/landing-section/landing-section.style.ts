c2749e7f0ebb7eaac519c89f0131074c38058e45
David-Itzhak-Kohen <114672677+David-Itzhak-Kohen@users.noreply.github.com>
2023-10-10 13:10:46 +0300

    more landing section

--- src/components/landing-section/landing-section.style.ts
+++ src/components/landing-section/landing-section.style.ts
@@ -1,9 +1,9 @@
-import { theme } from "src/styles/theme";
 import { createStyles } from "../../styles/create-styles";
 
 export const useStyles = () =>
   createStyles({
     container: {
+      maxWidth: "100%",
       position: "relative",
       display: "flex",
       flexDirection: "column",
@@ -11,21 +11,30 @@ export const useStyles = () =>
       justifyContent: "center",
     },
     info: {
+      maxWidth: "70vw",
       position: "absolute",
       flexDirection: "column",
       alignItems: "center",
       justifyContent: "center",
+      background: "rgba(255,255,255,0.1)",
+      backdropFilter: "blur(10px)",
+      borderRadius: "10px",
+      padding: "20px 30px",
+      border: "1px solid rgba(255,255,255,0.2)",
+      boxShadow: "10px 10px 30px rgba(0,0,0,0.2)",
     },
     title: {
       textAlign: "center",
-      fontSize: "36px",
+      fontSize: "2rem",
       fontWeight: 400,
+      filter: "drop-shadow(10px 10px 30px rgba(0,0,0,1))",
     },
     subtitle: {
       textAlign: "center",
-      fontSize: "24px",
+      fontSize: "1rem",
       fontWeight: 300,
       marginBottom: "20px",
+      filter: "drop-shadow(10px 10px 30px rgba(0,0,0,1))",
     },
     buttonContainer: {
       width: "100%",
--- src/components/landing-section/landing-section.tsx
+++ src/components/landing-section/landing-section.tsx
@@ -1,7 +1,34 @@
-import React, { useState } from "react";
+import React, { FC, useState } from "react";
 import { Button, Typography } from "@mui/material";
 import { useStyles } from "./landing-section.style";
 import { LANDING_MOCK } from "src/mock/landing.mock";
+import { useDeviceContext } from "src/context/device.context";
+import { ILandingSectionData } from "src/interfaces/landing-section.interface";
+
+const Background: FC<{ data: ILandingSectionData }> = ({ data }) => {
+  const { isMobile } = useDeviceContext();
+
+  if (isMobile) {
+    return (
+      <div
+        style={{
+          backgroundImage: `url(${data.image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+          width: "100%",
+        }}
+      />
+    );
+  }
+
+  return (
+    <video autoPlay id="background-video" loop muted poster={data.image} style={{ width: "100%" }}>
+      <source src={data.video} type="video/mp4" />
+    </video>
+  );
+};
 
 const LandingSection = () => {
   const styles = useStyles();
@@ -13,9 +40,7 @@ const LandingSection = () => {
 
   return (
     <div style={styles.container}>
-      <video autoPlay id="background-video" loop muted poster={data.image}>
-        <source src={data.video} type="video/mp4" />
-      </video>
+      <Background data={data} />
       <div style={styles.info}>
         <Typography style={styles.title}>{data.title}</Typography>
         <Typography style={styles.subtitle}>{data.subtitle}</Typography>
