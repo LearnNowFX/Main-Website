271bf1d8c59015cbe22734a6b7059acbb9b87372
David-Itzhak-Kohen <114672677+David-Itzhak-Kohen@users.noreply.github.com>
2023-10-10 17:29:51 +0300

    react query

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -1,23 +1,22 @@
+import React from "react";
+import { useQuery } from "react-query";
 import { useStyles } from "./product.style";
 import { Grid, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
 import Product from "src/components/product/product";
 import { IProduct } from "src/interfaces/product.interface";
 import { productService } from "src/services/product.service";
 
 const Products = () => {
   const styles = useStyles();
-  const [products, setProducts] = useState<IProduct[]>([]);
-
-  useEffect(() => {
-    productService.getAll().then(products => setProducts(products));
-  }, []);
+  const { data: products } = useQuery<IProduct[]>("products", () => productService.getAll(), {
+    refetchOnMount: false,
+  });
 
   return (
     <div style={styles.container}>
       <Typography style={styles.title}>Products</Typography>
       <Grid container spacing={3} style={styles.gridContainer}>
-        {products.map(product => (
+        {products?.map(product => (
           <Grid item xs={12} sm={12} md={6} lg={4} xl={4} key={product.id} style={styles.gridItem}>
             <Product key={product.id} product={product} />
           </Grid>
--- src/pages/tutorials/tutorials.tsx
+++ src/pages/tutorials/tutorials.tsx
@@ -1,25 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useQuery } from "react-query";
+import { useStyles } from "./tutorials.style";
+import { Grid, Typography } from "@mui/material";
 import { tutorialService } from "src/services/tutorial.service";
 import { IVideoDetails } from "src/interfaces/video-details.interface";
 import YouTubePlayer from "src/components/generic/youtube-player/youtube-player";
-import { Grid, Typography } from "@mui/material";
-import { useStyles } from "./tutorials.style";
 
 const Tutorials = () => {
   const styles = useStyles();
-  const [videos, setVideos] = useState<IVideoDetails[]>([]);
-
-  useEffect(() => {
-    tutorialService.getAll().then(videos => setVideos(videos));
-  }, []);
+  const { data: tutorials } = useQuery<IVideoDetails[]>(
+    "tutorials",
+    () => tutorialService.getAll(),
+    {
+      refetchOnMount: false,
+    }
+  );
 
   return (
     <div style={styles.container}>
       <Typography style={styles.title}>Tutorials</Typography>
       <Grid container spacing={3} style={styles.gridContainer}>
-        {videos.map(video => (
+        {tutorials?.map(video => (
           <Grid item xs={12} sm={12} md={6} lg={4} xl={4} key={video.id} style={styles.gridItem}>
-            <YouTubePlayer key={video.id} width={400} videoId={video.id} title={video.title} />
+            <YouTubePlayer key={video.id} videoId={video.id} title={video.title} />
           </Grid>
         ))}
       </Grid>
