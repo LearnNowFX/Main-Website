328e68fc37728ac1a3748734a9bc00125e685055
David-Itzhak-Kohen <114672677+David-Itzhak-Kohen@users.noreply.github.com>
2023-10-11 10:19:07 +0300

    layout highlights

--- src/components/highlights-section/highlights-section.tsx
+++ src/components/highlights-section/highlights-section.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 import Carousel from "../carousel/carousel";
 import { useStyles } from "./highlights-section.style";
 import { MOCK_FEATURED_ITEMS } from "src/mock/featured.mock";
@@ -7,30 +7,23 @@ import { MOCK_FEATURED_ITEMS } from "src/mock/featured.mock";
 const HighlightsSection = () => {
   const styles = useStyles();
 
-  const GRID_ITEM_PROPS = {
-    xs: 12,
-    sm: 12,
-    md: 12,
-    lg: 4,
-    xl: 4,
-  };
-
   return (
     <div style={styles.container}>
       <Typography style={styles.title}>FEATURED CONTENT</Typography>
-      <Grid container style={styles.gridContainer}>
-        <Grid item style={styles.gridItem} {...GRID_ITEM_PROPS}>
+      <Box sx={styles.gridContainer}>
+        <Box sx={styles.gridItem}>
           <div style={styles.linksSection}></div>
-        </Grid>
-        <Grid item style={styles.gridItem} {...GRID_ITEM_PROPS}>
-          <div style={styles.linksSection}>
+        </Box>
+        <Box sx={styles.gridItem}>
+          <div style={styles.carouselContainer}>
             <Carousel items={MOCK_FEATURED_ITEMS} />
           </div>
-        </Grid>
-        <Grid item style={styles.gridItem} {...GRID_ITEM_PROPS}>
+          {/* <TextField style={styles.searchField} placeholder="Search" /> */}
+        </Box>
+        <Box sx={styles.gridItem}>
           <div style={styles.linksSection}></div>
-        </Grid>
-      </Grid>
+        </Box>
+      </Box>
     </div>
   );
 };
